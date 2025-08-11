5b2183530adb3ebd6f3caea5c3f3f687187228f1
Maksim <m_telega@mail.ru>
2025-08-11 09:32:27 +0300

    chore: update FavoritesRelease.vue

--- src/renderer/views/favorites/components/release/FavoritesRelease.vue
+++ src/renderer/views/favorites/components/release/FavoritesRelease.vue
@@ -76,35 +76,53 @@ export default {
     VClamp,
     ReleaseProgress
   },
-  mounted() {
-    if (!window._epscache) {
-      window._epscache = new Map()
+  async mounted() {
+    window._epscache = window._epscache || new Map();
+  
+    const cachedData = window._epscache.get(this.release.code);
+    if (cachedData?.isValid) {
+      this.totalEpisodes = String(cachedData.data.episodes_total);
+      return;
     }
+  
+    try {
+      const endpoints = [
+        'https://aniliberty.top/api/v1/anime/releases',
+        'https://anilibria.top/api/v1/anime/releases',
+        'https://www.anilibria.top/api/v1/anime/releases'
+      ];
+  
+      const response = await this.fetchFromFirstAvailableEndpoint(endpoints, this.release.code);
+      
+      if (response) {
+        const data = await response.json();
 
-    const cache = window._epscache.get(this.release.code)
-    if (cache) {
-      cache.__ok__ = true
+        window._epscache.set(this.release.code, {
+          data,
+          isValid: true
+        });
+        this.totalEpisodes = String(data.episodes_total);
+      }
+    } catch (error) {
+      console.error('Failed to load episodes data:', error);
     }
-
-    const tryToFetchThisShit = await Promise.allSettled([
-      fetch(`https://aniliberty.top/api/v1/anime/releases/${this.release.code}`),
-      fetch(`https://anilibria.top/api/v1/anime/releases/${this.release.code}`),
-      fetch(`https://www.anilibria.top/api/v1/anime/releases/${this.release.code}`)
-    ])
-
-    const findOK = tryToFetchThisShit.find((x) => x.ok)
-    
-    Promise.resolve(cache || findOK)
-      .then(async x => {
-        if (cache && cache.__ok__){
-          x = cache
-        } else {
-          x = await x.json()
-          window._epscache.set(this.release.code, x)
+    },
+  methods: {
+    async fetchFromFirstAvailableEndpoint(endpoints, releaseCode) {
+      for (const endpoint of endpoints) {
+        try {
+          const url = `${endpoint}/${releaseCode}`;
+          const response = await fetch(url);
+          
+          if (response.ok) {
+            return response;
+          }
+        } catch (e) {
+          continue;
         }
-
-        this.totalEpisodes = String(x.episodes_total)
-      }).catch(() => {})
+      }
+      return null;
+    }
   },
   data () {
     return { totalEpisodes: '' }
