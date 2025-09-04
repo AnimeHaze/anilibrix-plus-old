ed73a63dff54339dfbb93b63f645b386c5395aaf
viiprogrammer <m_telega@mail.ru>
2025-09-04 22:36:38 +0300

    Revert "chore: update ReleaseView.vue"
    This reverts commit 8dd71014d207bd5e665345021d668c126f6432b3.

--- src/renderer/views/release/ReleaseView.vue
+++ src/renderer/views/release/ReleaseView.vue
@@ -399,17 +399,18 @@ export default {
     async fetchAdditional() {
       this.loadingAdditional = true;
       try {
-        const endpoints = [
-          'https://aniliberty.top/api/v1/anime/franchises/release',
-          'https://anilibria.top/api/v1/anime/franchises/release',
-          'https://www.anilibria.top/api/v1/anime/franchises/release'
-        ];
-    
-        const response = await this.fetchFromFirstAvailableEndpoint(endpoints, this.releaseId);
-        
-        if (!response) throw new Error('Failed to load franchises');
 
-        const franchisesData = await response.json();
+        const tryToFetchThisShit = await Promise.allSettled([
+          fetch(`https://aniliberty.top/api/v1/anime/franchises/release/${this.releaseId}`),
+          fetch(`https://anilibria.top/api/v1/anime/franchises/release/${this.releaseId}`),
+          fetch(`https://www.anilibria.top/api/v1/anime/franchises/release/${this.releaseId}`)
+        ])
+    
+        const findOK = tryToFetchThisShit.find((x) => x.ok)
+
+        if (!findOK) throw new Error('Failed to load franchises');
+
+        const franchisesData = await findOK.json();
 
         if (franchisesData.length > 0) {
           const franchise = franchisesData[0];
@@ -440,23 +441,6 @@ export default {
       }
     },
 
-
-    async fetchFromFirstAvailableEndpoint(endpoints, releaseId) {
-      for (const endpoint of endpoints) {
-        try {
-          const url = `${endpoint}/${releaseId}`;
-          const response = await fetch(url);
-          
-          if (response.ok) {
-            return response;
-          }
-        } catch (e) {
-          continue;
-        }
-      }
-      return null;
-    },
-    
     extractReleaseIds(franchises) {
       const releaseIds = new Set()
       franchises.forEach(franchise => {
