6b88b781d5538316f08acc2fc6d823d5655b5ff4
viiprogrammer <m_telega@mail.ru>
2025-09-04 22:36:34 +0300

    Revert "chore: update ReleaseView.vue"
    This reverts commit 821b7135cafe16ea964d7aff9b8c19cda3d853a4.

--- src/renderer/views/release/ReleaseView.vue
+++ src/renderer/views/release/ReleaseView.vue
@@ -449,8 +449,6 @@ export default {
           
           if (response.ok) {
             return response;
-          } else {
-            continue;
           }
         } catch (e) {
           continue;
