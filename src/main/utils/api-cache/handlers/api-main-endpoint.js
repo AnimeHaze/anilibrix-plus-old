726c28995032896b5473c1f9ac40f85ce6489346
viiprogrammer <m_telega@mail.ru>
2026-01-07 19:31:11 +0300

    fix: unhandled error when fail to fallback to cache

--- src/main/utils/api-cache/handlers/api-main-endpoint.js
+++ src/main/utils/api-cache/handlers/api-main-endpoint.js
@@ -1,48 +1,56 @@
 export default function (apiController, cacheService) {
   return async (req, res, next) => {
-    const { query } = req.body;
+    try {
+      const { query } = req.body;
 
-    if (['user', 'favorites'].includes(query)) {
-      let response = null
-      if (query === 'favorites' && req.body.action) {
-        console.log('Favorites action received', req.body)
-        response = await apiController.handleFavoritesProxy(req.body.action, req.body.id)
-      } else {
-        response = await apiController.handleProxyWithCache(query, {
-          ...req.body
-        })
+      if (['user', 'favorites'].includes(query)) {
+        let response = null
+        if (query === 'favorites' && req.body.action) {
+          console.log('Favorites action received', req.body)
+          response = await apiController.handleFavoritesProxy(req.body.action, req.body.id)
+        } else {
+          response = await apiController.handleProxyWithCache(query, {
+            ...req.body
+          })
+        }
+
+        if (query === 'favorites' && !response.error && !req.body.action) {
+          response.data.items.forEach((v, i) => {
+            v.total_series = cacheService.releases.get(v.id)?.series || '(0)'
+          })
+        }
+
+        if (response.error) {
+          res.status(400).send(response);
+        } else {
+          res.send(response);
+        }
+
+        return
       }
 
-      if (query === 'favorites' && !response.error && !req.body.action) {
-        response.data.items.forEach((v, i) => {
-          v.total_series = cacheService.releases.get(v.id)?.series || '(0)'
-        })
+      if (!['list', 'release', 'catalog', 'random_release', 'search', 'years', 'genres'].includes(query)) {
+        res.status(404).send({
+          error: 'Endpoint not found',
+          status: false
+        });
+
+        return
       }
+
+      const response = await apiController.handleRequest(req.body, query);
 
       if (response.error) {
         res.status(400).send(response);
       } else {
         res.send(response);
       }
-
-      return
-    }
-
-    if (!['list', 'release', 'catalog', 'random_release', 'search', 'years', 'genres'].includes(query)) {
-      res.status(404).send({
-        error: 'Endpoint not found',
+    } catch (e) {
+      console.error(e)
+      res.status(400).send({
+        error: 'Internal server error',
         status: false
       });
-
-      return
-    }
-
-    const response = await apiController.handleRequest(req.body, query);
-
-    if (response.error) {
-      res.status(400).send(response);
-    } else {
-      res.send(response);
     }
   }
 }
